cac7393eddd3b5880e214f56691e2f12b5b58f30
Vinicius Mesel <vinicius@bigsp.com.br>
2018-08-05 22:32:26 -0300

    Adiciona o cliente WIFI no ESP

---
+++ src/client.c
@@ -0,0 +1,17 @@
+
+#include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
+
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+
+void setup() {
+  WiFiManager wifiManager;
+  wifiManager.autoConnect();
+
+}
+
+// the loop function runs over and over again forever
+void loop() {
+
+}
